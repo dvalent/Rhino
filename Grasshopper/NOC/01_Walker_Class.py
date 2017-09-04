cdc0c1a027fc503b98e534ad3608cc9ee51647e9
dvalent <david.valent@hok.com>
2017-09-04 10:39:57 +0100

    New File Struct
    New File Struct

---
+++ Grasshopper/NOC/01_Walker_Class.py
@@ -0,0 +1,36 @@
+﻿import rhinoscriptsyntax as rs
+import random as r
+
+r.seed(seed)
+
+class Walker:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        
+    def point(self):
+        shape = rs.AddPoint(self.x,self.y,0)
+        return shape
+        
+    def step(self):
+        choice = r.randint(0,3)
+        if choice == 0:
+            self.x += 1
+        elif choice == 1:
+            self.x -= 1
+        elif choice == 2:
+            self.y += 1
+        else:
+            self.y -= 1
+            
+            
+w = Walker()
+
+pList = []
+
+for t in range(time):
+    w.step()
+    pList.append(w.point())
+
+a = pList
+print pList
